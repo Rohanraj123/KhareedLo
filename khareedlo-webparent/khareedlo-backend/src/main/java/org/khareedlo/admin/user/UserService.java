ad6053e88cea8e11d31ca91ea6227b666749b007
Rohanraj123 <rajrohan88293@gmail.com>
2024-07-09 18:01:09 +0530

    Some issue

---
+++ khareedlo-webparent/khareedlo-backend/src/main/java/org/khareedlo/admin/user/UserService.java
@@ -0,0 +1,22 @@
+package org.khareedlo.admin.user;
+
+import org.khareedlo.common.entity.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public List<User> listAll() {
+        return (List<User>) userRepository.findAll();
+    }
+}
