856fbaefe21809e820e8e8aa83e22343add77ce4
Rohanraj123 <rajrohan88293@gmail.com>
2024-07-09 13:32:40 +0530

    Built Admin homepage nav bar and made it responsive

---
+++ khareedlo-webparent/khareedlo-frontend/src/main/java/org/khareedlo/site/KhareedLoFrontendApplication.java
@@ -0,0 +1,11 @@
+package org.khareedlo.site;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KhareedLoFrontendApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(KhareedLoFrontendApplication.class);
+    }
+}
