11c0e7ee16517d996a6bab343d49a971369d57a2
Rohanraj123 <rajrohan88293@gmail.com>
2024-07-10 08:20:55 +0530

    Password Encoder implemented correctly

--- khareedlo-webparent/khareedlo-backend/src/main/java/org/khareedlo/admin/KhareedLoBackendApplication.java
+++ khareedlo-webparent/khareedlo-backend/src/main/java/org/khareedlo/admin/KhareedLoBackendApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
-@EntityScan({"org.khareedlo.common.entity", "org.khareedlo.admin.user"})
+@EntityScan({"org.khareedlo.common.entity", "org.khareedlo.admin.user", "org.khareedlo.admin.security"})
 public class KhareedLoBackendApplication {
     public static void main(String[] args) {
         SpringApplication.run(KhareedLoBackendApplication.class, args);
